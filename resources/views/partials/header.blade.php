dcd4db2e801a75ebc39cdf96478e1c080c4e16bb
LeonidVilcu <vilcu.leo@mail.ru>
2023-03-09 23:47:54 +0100

    first commit

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,63 @@
+<section class="bg-primary">
+    <div class="container">
+        <div class="text-end text-white">
+            <span class="mx-5">DC POWER VISA</span>
+            <span>ADDITIONAL DC SITES</span>
+        </div>
+    </div>
+</section>
+<section>
+    <div class="container">
+        <nav class="navbar navbar-expand-lg bg-light">
+            <div class="container-fluid">
+                <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse " id="navbarText">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0 ">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Features</a>
+                        </li>
+                         <li class="nav-item">
+                           <a class="nav-link" href="#">Features</a>
+                         </li>
+                         <li class="nav-item">
+                           <a class="nav-link" href="#">Features</a>
+                         </li>
+                         <li class="nav-item">
+                           <a class="nav-link" href="#">Features</a>
+                         </li>
+                         <li class="nav-item">
+                           <a class="nav-link" href="#">Features</a>
+                         </li>
+                         <li class="nav-item">
+                           <a class="nav-link" href="#">Features</a>
+                         </li>
+                         <li class="nav-item">
+                           <a class="nav-link" href="#">Features</a>
+                         </li>
+                         <li class="nav-item">
+                           <a class="nav-link" href="#">Features</a>
+                         </li>
+                         <li class="nav-item">
+                           <a class="nav-link" href="#">Features</a>
+                         </li>
+                         <li class="nav-item">
+                           <a class="nav-link" href="#">Features</a>
+                         </li>
+                    </ul>
+                </div>
+                <form class="d-flex" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                </form>
+
+
+            </div>
+          </nav>
+    </div>
+
+</section>
+    
+    
+</section>
